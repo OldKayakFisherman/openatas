0c945a39e0977f472da31fe9e2f660228401f521
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-03-11 18:55:08 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -26,7 +26,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True, index=True)
     username = db.Column(db.String(100), unique=True, index=True)
     password_hash = db.Column(db.String(256))
-    
+    approved = db.Column(db.Boolean)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    role = db.relationship("Role", backref="users")
 
     @property
     def password(self):
@@ -61,6 +63,58 @@ class CFDAMappings(db.Model):
     cfda_ext = db.Column(db.String(20))
     division_id = db.Column(db.Integer, db.ForeignKey("operating_divisions.id"))
     division = db.relationship("OperatingDivision", backref="cfda_mappings")
+
+
+class NotificationQueue(db.Model):
+
+    __tablename__ = "notification_queue"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(40))
+    date_sent = db.Column(db.DateTime)
+    date_queued = db.Column(db.DateTime)
+    subject = db.Column(db.Text)
+    body = db.Column(db.Text)
+
+class NotificationQueueAttachment(db.Model):
+
+    __tablename__ = "notification_queue_attachments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    mime_type = db.Column(db.String(40))
+    extension = db.Column(db.String(10))
+    filename = db.Column(db.String(255))
+    filecontents = db.Column(db.LargeBinary)
+    notification_id = db.Column(db.Integer, db.ForeignKey("notification_queue.id"))
+    notification = db.relationship("NotificationQueue", backref="notification_queue_attachments")
+
+class StoredDocuments(db.Model):
+
+    __tablename__ = "stored_documents"
+
+    id = db.Column(db.Integer, primary_key=True)
+    mime_type = db.Column(db.String(40))
+    extension = db.Column(db.String(10))
+    filename = db.Column(db.String(255))
+    filecontents = db.Column(db.LargeBinary)
+
+class AuditLog(db.Model):
+    
+    __tablename__ = "event_logs"
+ 
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    host = db.Column(db.String, nullable=False)
+    message = db.Column(db.String)
+    severity = db.Column(db.String)
+    type = db.Column(db.String)
+    whencreated = db.Column(db.DateTime)
+    whocreated = db.Column(db.String)
+    success = db.Column(db.Boolean)
+    source= db.Column(db.String)
+    request_headers= db.Column(db.String)
+    clientversion= db.Column(db.String)
+    backendversion= db.Column(db.String) 
 
 
 
