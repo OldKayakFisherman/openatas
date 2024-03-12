5ea9e4f928e233119baf9c265ae7102c5d600632
Rick Flagg <richard.lee.flagg@outlook.com>
2024-03-12 11:14:31 -0400

    removing migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData 
 from config import config
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention={
+    'pk': 'pk_%(table_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'ix': 'ix_%(table_name)s_%(column_0_name)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+}))
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
